049062fac4037fcceab49a620d8f8bbe57427278
Karim Kanji <77146303+KarimKanji@users.noreply.github.com>
2024-01-13 14:56:14 +0200

    Add files via upload

---
+++ Assignment 3/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int q = 110;
+        double w = 45.5;
+        char h = 'g';
+
+
+        System.out.println("Heltalsvariabeln har värdet:" + q);
+        System.out.println("Flyttalsvariablen har värdet:" + w);
+        System.out.println("Teckenvariabeln har värdet:" + h);
+
+    }
+}
