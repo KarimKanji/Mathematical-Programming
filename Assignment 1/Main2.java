cba826e8880ffd61b3a6d0095a9e16451b92d70f
Karim Kanji <77146303+KarimKanji@users.noreply.github.com>
2024-01-13 14:54:44 +0200

    Add files via upload
    Basic print in Java

---
+++ Assignment 1/Main2.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Päron, äppel, bananer, mandariner och apelsiner");
+    }
+}
