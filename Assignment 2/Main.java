00df299dfc20c4f602cead7526a3cbbe4b36b869
Karim Kanji <77146303+KarimKanji@users.noreply.github.com>
2024-01-13 14:55:21 +0200

    Add files via upload

---
+++ Assignment 2/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("\"Att vara elle icke vara\" -det är frågan!");
+        System.out.println();
+        System.out.println("Sade 'Shakespeare' för länge sedan,");
+        System.out.println("Vilket stämde igår /\\ och ännu idag");
+    }
+}
